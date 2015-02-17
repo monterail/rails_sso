35e1573cd44ae3a72f4817367ece149b06c20c4c
Jan Dudulski <jan@dudulski.pl>
2015-02-17 17:01:20 +0100

    [test] Move CodeClimate test reporter to the very first line

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
@@ -5,8 +7,6 @@ require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
 require "rails/test_help"
 require "mocha/mini_test"
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 
 # Filter out Minitest backtrace while allowing backtrace from other libraries
 # to be shown.
