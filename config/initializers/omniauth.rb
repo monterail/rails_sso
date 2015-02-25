32ca6c263e43234cbb1bf7a4c0937fa3c580a8e8
Jan Dudulski <jan@dudulski.pl>
2015-02-25 11:45:32 +0100

    [fix] Initialize provider
    * do not call omniauth builder during generation process
    * call omniauth builder after any app initializer

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider RailsSso.provider_name,
-    RailsSso.provider_key,
-    RailsSso.provider_secret,
-    callback_path: RailsSso.provider_callback_path
-end
--- lib/rails_sso/engine.rb
+++ lib/rails_sso/engine.rb
@@ -5,5 +5,16 @@ module RailsSso
         include RailsSso::Helpers
       end
     end
+
+    initializer 'sso.omniauth', after: :load_config_initializers, before: :build_middleware_stack do |app|
+      if RailsSso.provider_name
+        app.config.middleware.use OmniAuth::Builder do
+          provider RailsSso.provider_name,
+            RailsSso.provider_key,
+            RailsSso.provider_secret,
+            callback_path: RailsSso.provider_callback_path
+        end
+      end
+    end
   end
 end
